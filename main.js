8087cb1861eeda99f035a87d05c87f590c152a90
Laylalll <yusioulai@gmail.com>
2023-05-21 16:58:16 +0800

    add back style & filp a card function

--- main.js
+++ main.js
@@ -14,22 +14,35 @@ view = {
     cardsTable.innerHTML = rawHtml
   },
 
-  getCardElement(index) {
+  getCardContent(index) { //取得牌正面元素
     // index
     // 0~12 => 黑桃1~13
     // 13~25 => 紅心1~13
     // 26~38 => 方塊1~13
     // 39~51 => 梅花1~13
-    const number = this.transformNumber((index % 13) + 1)
-    const symbol = Symbols[Math.floor(index / 13)]
+    const number = this.transformNumber((index % 13) + 1) //運算 number + 特殊數字轉換
+    const symbol = Symbols[Math.floor(index / 13)] //運算花色
     let rawHtml = `
-      <div class="card">
         <p>${number}</p>
         <img src="${symbol}" alt="">
         <p>${number}</p>
-      </div>
     `
     return rawHtml
+  },
+
+  getCardElement(index) { //取得牌背面元素 & index
+    return `<div class="card back" data-index=${index}></div>`
+  },
+
+  flipCard(card) {
+    // 回傳正面
+    if (card.classList.contains('back')) {
+      card.classList.remove('back')
+      return card.innerHTML = this.getCardContent(Number(card.dataset.index))
+    }
+    // 回傳背面
+    card.classList.add('back')
+    card.innerHTML = null
   },
 
   transformNumber(number) {
@@ -50,7 +63,8 @@ view = {
 
 const utility = {
   getRandomNumberArray(count) {
-    const array = Array.from(Array(count).keys())
+    const array = Array.from(Array(count).keys()) //建立指定元素數量(0~51)的陣列
+    //從底牌開始，將它抽出，與前面任一張牌交換，(此動作重複執行到倒數第二張牌)
     for (let index = array.length - 1; index > 0; index--) {
       let randomIndex = Math.floor(Math.random() * (index + 1))
         ;[array[index], array[randomIndex]] = [array[randomIndex], array[index]]
@@ -62,3 +76,9 @@ const utility = {
 
 // EXECUTING /////////////////////////////////////////
 view.displayCards(utility.getRandomNumberArray(52))
+
+document.querySelectorAll('.card').forEach(
+  card => card.addEventListener('click', function (event) {
+    view.flipCard(card)
+  })
+)
